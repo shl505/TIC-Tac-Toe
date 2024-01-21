be4c56f6b6bf42e8b46627c4b75a268be26e128b
shloimy <136237874+shl505@users.noreply.github.com>
2024-01-21 21:02:47 +0200

    Update TIC_Tac_Toe.py

--- TIC_Tac_Toe.py
+++ TIC_Tac_Toe.py
@@ -1,8 +1,19 @@
-class game:
-  pass
+class Game:
+  def __init__(self):
+    pass
   
-class player:
-  pass
+class Player:
+  def __init__(self, name, player_shape):
+    self.name = name
+    self.player_shape = player_shape
 
 class Board:
-  pass
+  def __init__(self):
+    self.board = [' '] * 9
+
+  def to_string(self):
+    return '{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}\n'.format(*self.board)
+
+  def make_move(self, player, plase):
+    if self.board[plase] == ' ':
+      self.board[plase] = self.player_shape
